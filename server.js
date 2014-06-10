e08dc87f8be4fd719abb031189a12b881f01c6dc
Viktor Leonets <4405511@gmail.com>
2014-06-11 00:16:23 +0300

    add S3

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ server.use(prerender.blacklist());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
-// server.use(prerender.s3HtmlCache());
+server.use(prerender.s3HtmlCache());
 
 server.start();
